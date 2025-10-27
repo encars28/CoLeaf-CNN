73161f89163e2d3cdc0d14ad03fab8b5058fda5a
encar <encars28@gmail.com>
2025-10-27 21:37:52 +0100

    first model trained and first results

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 from torch.utils.data import Dataset
+from tqdm.notebook import tqdm
 
 class CoLeafDataset(Dataset):
     def __init__(self, data_dir, transform=None):
@@ -9,28 +10,23 @@ class CoLeafDataset(Dataset):
         self.classes = sorted([d for d in os.listdir(data_dir)])
         self.class_to_idx = {cls_name: i for i, cls_name in enumerate(self.classes)}
         
-        self.image_paths = []
+        self.images = []
         self.labels = []
         
-        for class_name in self.classes:
+        for class_name in tqdm(self.classes, desc="Loading classes"):
             class_path = os.path.join(self.data_dir, class_name)
-            for filename in os.listdir(class_path):
-                self.image_paths.append(os.path.join(class_path, filename))
+            for filename in tqdm(os.listdir(class_path), desc=f"Loading images for class {class_name}", leave=False):
+                image = Image.open(os.path.join(class_path, filename)).convert('RGB')
+                # Aplicar las transformaciones
+                if self.transform:
+                    image = self.transform(image)
+                
+                self.images.append(image)
                 self.labels.append(self.class_to_idx[class_name])
 
     def __len__(self):
-        return len(self.image_paths)
+        return len(self.images)
     
     def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
-        label = self.labels[idx]
-
-        # Cargar la imagen y convertirla a RGB (por si acaso)
-        image = Image.open(img_path).convert('RGB')
-        
-        # Aplicar las transformaciones
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
+        return self.images[idx], self.labels[idx]
 
--- network.py
+++ network.py
@@ -9,15 +9,15 @@ class Net(nn.Module):
     # then, after a 2x2 pooling, the height and width are halved
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5) # ajustado a 112 canales, ya que maxpool reduce el tamaño a la mitad
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 53 * 53, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10) # 10 es el numero de clases
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = flatten(x, 1) # flatten all dimensions except batch
+        x = flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
